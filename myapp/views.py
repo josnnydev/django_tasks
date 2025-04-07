65d8e882704d6a384f86ffeccff91ecfea23353b
josnnydev <pjosnny@gmail.com>
2025-04-07 17:38:52 -0400

    update

--- myapp/views.py
+++ myapp/views.py
@@ -3,23 +3,33 @@ from django.http import HttpResponse, JsonResponse
 from myapp.forms import CreateTaskForm,CreateProjectForm 
 from myapp.models import Project, Task
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
  
  
 def index(request):
     return render(request, 'index.html',status=200 )
 
-def signup(request ):
-    form = UserCreationForm()  
-    try:
-        if request.method == 'POST':
-            form = UserCreationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('create_task')
-    except:
-         return render(request, 'signup/signup.html', {'form': form,'error': 'An error occurred while creating the user.'}) 
-              
-    return render(request, 'signup/signup.html', {'form': form})   
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)   
+            return redirect('create_task')
+        else:
+            return render(request, 'signup/signup.html', {
+                'form': form,
+                'error': 'Please correct the errors below.'
+            })
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup/signup.html', {'form': form})
+  
      
 
 
